1493caeb0aa98b0d9044babea1978b2b3a951560
win-nce <jumikirali@gmail.com>
2025-10-08 21:18:44 +0500

    feat: added swiper js for react

--- src/pages/home/banner.tsx
+++ src/pages/home/banner.tsx
@@ -1,29 +1,104 @@
 import { Container } from "../../widgets/container"
 import BannerImage from "./../../assets/banner.png";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
 
 export const Banner = () => {
     return <section className="pt-[18px] pb-[56px]">
         <Container>
-            <div className="bg-[#FFF1B8] rounded-[16px] px-[70px] py-[68px] relative">
+            <Swiper spaceBetween={25} slidesPerView={1}>
+                <SwiperSlide>
+                    <div className="bg-[#FFF1B8] rounded-[16px] px-[70px] py-[68px] relative">
 
-                <div className="space-y-4 w-[538px]">
-                    <h2 className="text-[39px] font-bold">
-                        Онлайн-сервис доставки еды на дом
-                    </h2>
+                        <div className="space-y-4 w-[538px]">
+                            <h2 className="text-[39px] font-bold">
+                                Онлайн-сервис доставки еды на дом
+                            </h2>
 
-                    <p className="text-2xl">
-                        Блюда из любимого ресторана привезет курьер в перчатках, маске и с антисептиком
-                    </p>
-                </div>
+                            <p className="text-2xl">
+                                Блюда из любимого ресторана привезет курьер в перчатках, маске и с антисептиком
+                            </p>
+                        </div>
 
-                <div className="absolute right-0 top-0 bottom-0">
-                    <img
-                        className="w-full h-full object-cover"
-                        src={BannerImage}
-                        alt="баннер"
-                    />
-                </div>
-            </div>
+                        <div className="absolute right-0 top-0 bottom-0">
+                            <img
+                                className="w-full h-full object-cover"
+                                src={BannerImage}
+                                alt="баннер"
+                            />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="bg-[#FFF1B8] rounded-[16px] px-[70px] py-[68px] relative">
+
+                        <div className="space-y-4 w-[538px]">
+                            <h2 className="text-[39px] font-bold">
+                                Онлайн-сервис доставки еды на дом
+                            </h2>
+
+                            <p className="text-2xl">
+                                Блюда из любимого ресторана привезет курьер в перчатках, маске и с антисептиком
+                            </p>
+                        </div>
+
+                        <div className="absolute right-0 top-0 bottom-0">
+                            <img
+                                className="w-full h-full object-cover"
+                                src={BannerImage}
+                                alt="баннер"
+                            />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="bg-[#FFF1B8] rounded-[16px] px-[70px] py-[68px] relative">
+
+                        <div className="space-y-4 w-[538px]">
+                            <h2 className="text-[39px] font-bold">
+                                Онлайн-сервис доставки еды на дом
+                            </h2>
+
+                            <p className="text-2xl">
+                                Блюда из любимого ресторана привезет курьер в перчатках, маске и с антисептиком
+                            </p>
+                        </div>
+
+                        <div className="absolute right-0 top-0 bottom-0">
+                            <img
+                                className="w-full h-full object-cover"
+                                src={BannerImage}
+                                alt="баннер"
+                            />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="bg-[#FFF1B8] rounded-[16px] px-[70px] py-[68px] relative">
+
+                        <div className="space-y-4 w-[538px]">
+                            <h2 className="text-[39px] font-bold">
+                                Онлайн-сервис доставки еды на дом
+                            </h2>
+
+                            <p className="text-2xl">
+                                Блюда из любимого ресторана привезет курьер в перчатках, маске и с антисептиком
+                            </p>
+                        </div>
+
+                        <div className="absolute right-0 top-0 bottom-0">
+                            <img
+                                className="w-full h-full object-cover"
+                                src={BannerImage}
+                                alt="баннер"
+                            />
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+
         </Container>
     </section>
 }
